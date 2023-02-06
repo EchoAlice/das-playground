82786c802a0699553121e45b071a5080a03dd59c
EchoAlice <echoalice0@gmail.com>
2023-02-06 18:14:49 +0000

    Implement naive SecureDASContentKey.  Compress later

--- src/das_node/content_key.rs
+++ src/das_node/content_key.rs
@@ -16,7 +16,15 @@ use ssz_derive::{Decode, Encode};
 // Discv5 TalkReq/TalkResp uses these keys to know which overlay-specific req handling logic is needed
 
 // To Do:
-// Create the SecureDASContentKey!  Can I just create a SecureDASContentKey and implement the same methods for both keys at the same time?
+// Figure out how I can aggregate the two keys logic
+
+// Can I pass TContentKey into our implementations?  If this works, we'll be able to plug in TContentKey instead of doubling up logic here
+// and within our overlay.rs!
+// pub enum TContentKey {
+//     DASContentKey,
+//     SecureDASContent,
+// }
+
 
 /// This is a content key in the DAS overlay network.
 #[derive(Clone, Debug, Decode, Encode, PartialEq)]
@@ -24,7 +32,6 @@ use ssz_derive::{Decode, Encode};
 pub enum DASContentKey {
     Sample([u8; 32]),
 }
-
 
 #[allow(clippy::from_over_into)]
 impl Into<Vec<u8>> for DASContentKey {
@@ -79,6 +86,74 @@ impl Validator<DASContentKey> for DASValidator {
     {
         match content_key {
             DASContentKey::Sample(_) => Ok(()),
+        }
+    }
+}
+
+
+
+
+
+/// This is a content key in the SecureDAS overlay network.
+#[derive(Clone, Debug, Decode, Encode, PartialEq)]
+#[ssz(enum_behaviour = "union")]
+pub enum SecureDASContentKey {
+    Sample([u8; 32]),
+}
+
+#[allow(clippy::from_over_into)]
+impl Into<Vec<u8>> for SecureDASContentKey {
+    fn into(self) -> Vec<u8> {
+        self.as_ssz_bytes()
+    }
+}
+
+impl TryFrom<Vec<u8>> for SecureDASContentKey {
+    type Error = &'static str;
+
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        match SecureDASContentKey::from_ssz_bytes(&value) {
+            Ok(key) => Ok(key),
+            Err(_err) => {
+                println!("unable to decode SecureDASContentKey");
+                Err("Unable to decode SSZ")
+            }
+        }
+    }
+}
+
+impl Display for SecureDASContentKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = match self {
+            Self::Sample(b) => format!("sample: {}", hex::encode(b)),
+        };
+
+        write!(f, "{}", s)
+    }
+}
+
+impl OverlayContentKey for SecureDASContentKey {
+    fn content_id(&self) -> [u8; 32] {
+        match self {
+            SecureDASContentKey::Sample(b) => b.clone(),
+        }
+    }
+}
+
+pub struct SecureDASValidator;
+
+#[async_trait]
+impl Validator<SecureDASContentKey> for SecureDASValidator {
+    async fn validate_content(
+        &self,
+        content_key: &SecureDASContentKey,
+        content: &[u8],
+    ) -> anyhow::Result<()>
+// where
+        //     SecureDASContentKey: 'async_trait,
+    {
+        match content_key {
+            SecureDASContentKey::Sample(_) => Ok(()),
         }
     }
 }
