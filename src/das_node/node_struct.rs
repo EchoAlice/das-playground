83f425294ef07ba7361a73614a373eeee6f10533
EchoAlice <echoalice0@gmail.com>
2023-02-09 13:39:24 +0000

    Clean up node_struct.rs code

--- src/das_node/node_struct.rs
+++ src/das_node/node_struct.rs
@@ -9,22 +9,15 @@ use discv5_overlay::{
 use std::sync::Arc;
 
 use crate::das_node::content_key::{
-    // TContentKey,
-    // TValidator, 
     DASContentKey, 
     DASValidator,
 };
 
 
-// Should our DASNode contain generics?  Or should all generics be used when creating our individual overlays?
-// Get rid of generics from struct and impl definitions if they're not necessary!
 #[derive(Clone)]
 // pub struct DASNode<TContentKey, TValidator> {
 pub struct DASNode {
     pub discovery: Arc<Discovery>,
-    // I don't think this is correct
-    // pub overlay: Arc<OverlayProtocol<TContentKey, XorMetric, TValidator, MemoryContentStore>>,
-    
     pub overlay: Arc<OverlayProtocol<DASContentKey, XorMetric, DASValidator, MemoryContentStore>>,
     // pub secure_overlay: Arc<OverlayProtocol<SecureDASContentKey, XorMetric, SecureDASValidator, MemoryContentStore>>,
     
@@ -33,11 +26,9 @@ pub struct DASNode {
 }
 
 // The DASNode within Model-DAS returns itself AND an overlay service!
-// impl<TContentKey, TValidator> DASNode<TContentKey, TValidator> {
 impl DASNode {
     pub fn new(
         discovery: Arc<Discovery>,
-        // overlay: Arc<OverlayProtocol<TContentKey, XorMetric, TValidator, MemoryContentStore>>,
         overlay: Arc<OverlayProtocol<DASContentKey, XorMetric, DASValidator, MemoryContentStore>>,
         // secure_overlay: Arc<OverlayProtocol<SecureDASContentKey, XorMetric, SecureDASValidator, MemoryContentStore>>,
     ) -> Self {
